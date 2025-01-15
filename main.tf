be2c14b79e0e3853069c4670a72521e9cf620420
Alexandre Andrade <63659325+andrdal@users.noreply.github.com>
2025-01-15 11:33:22 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,168 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  instance_tenancy     = "default"
+  tags = {
+    IsUsedForDeploy = "False"
+    Name            = "VPC-AFT-${var.environment_name}"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ map_public_ip_on_launch = true
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ tags = {
+   Name = "${var.environment_name}-Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ tags = {
+   Name = "${var.environment_name}-Private Subnet ${count.index + 1}"
+ }
+}
+
+# Create the internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+   Name = "VPC-AFT-${var.environment_name}-IG"
+ }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment_name}-Public Route Table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment_name}-Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(aws_subnet.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+}
+
+resource "aws_eip" "nat_gateway" {
+  domain = "vpc"
+}
+
+resource "aws_route" "private_subnet_default_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+}
+
+resource "aws_route" "public_subnet_default_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_security_group" "ssm_sg" {
+  description = "VPC Endpoint Ports Required"
+  name        = "My SG Group for VPC SSM Endpoints"
+  tags        = {}
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 443
+    protocol  = "tcp"
+    to_port   = 443
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm_endpoint" {
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.ssm"
+  subnet_ids        = aws_subnet.private_subnets.*.id
+  security_group_ids = [aws_security_group.ssm_sg.id]
+  policy            = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "*", 
+      "Effect": "Allow", 
+      "Principal": "*", 
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_vpc_endpoint" "ec2messages_endpoint" {
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.ec2messages"
+  subnet_ids        = aws_subnet.private_subnets.*.id
+  security_group_ids = [aws_security_group.ssm_sg.id]
+  policy            = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "*", 
+      "Effect": "Allow", 
+      "Principal": "*", 
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_vpc_endpoint" "ssmmessages_endpoint" {
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.ssmmessages"
+  subnet_ids        = aws_subnet.private_subnets.*.id
+  security_group_ids = [aws_security_group.ssm_sg.id]
+  policy            = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "*", 
+      "Effect": "Allow", 
+      "Principal": "*", 
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
